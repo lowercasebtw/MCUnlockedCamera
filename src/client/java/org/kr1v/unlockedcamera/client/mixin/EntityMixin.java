a09352701e660d7340b9123d2a3145fa6f3e663d
kr1viah_ <kravpolet@gmail.com>
2025-03-20 17:13:56 +0100

    Fixed inverted mouse movement

--- src/client/java/org/kr1v/unlockedcamera/client/mixin/EntityMixin.java
+++ src/client/java/org/kr1v/unlockedcamera/client/mixin/EntityMixin.java
@@ -16,8 +16,9 @@ public abstract class EntityMixin {
      */
     @Overwrite
     public void setAngles(float yaw, float pitch) {
+        float normalizedPitch = ((pitch + 180) % 360 + 360) % 360 - 180;
         ((Entity)(Object)this).setYaw(yaw);
-        ((Entity)(Object)this).setPitch(pitch);
+        ((Entity)(Object)this).setPitch(normalizedPitch);
         ((Entity)(Object)this).prevYaw = ((Entity)(Object)this).getYaw();
         ((Entity)(Object)this).prevPitch = ((Entity)(Object)this).getPitch();
     }
@@ -29,10 +30,15 @@ public abstract class EntityMixin {
     public void changeLookDirection(double cursorDeltaX, double cursorDeltaY) {
         float f = (float)cursorDeltaY * 0.15F;
         float g = (float)cursorDeltaX * 0.15F;
-        if ((pitch < -90 || pitch > 90) && UnlockedCameraConfigManager.getConfig().shouldInvertMouse) ((Entity)(Object)this).setYaw(((Entity)(Object)this).getYaw() - g);
-        else ((Entity)(Object)this).setYaw(((Entity)(Object)this).getYaw() + g);
-        if ((pitch < -90 || pitch > 90) && UnlockedCameraConfigManager.getConfig().shouldInvertMouse) ((Entity)(Object)this).prevYaw -= g;
-        else ((Entity)(Object)this).prevYaw += g;
+        float normalizedPitch = ((pitch % 360) + 360) % 360;
+        if (normalizedPitch > 90 && normalizedPitch < 270) {
+            ((Entity)(Object)this).setYaw(((Entity)(Object)this).getYaw() - g);
+            ((Entity)(Object)this).prevYaw -= g;
+        }
+        else {
+            ((Entity) (Object) this).setYaw(((Entity) (Object) this).getYaw() + g);
+            ((Entity) (Object) this).prevYaw += g;
+        }
         ((Entity)(Object)this).setPitch(((Entity)(Object)this).getPitch() + f);
         ((Entity)(Object)this).prevPitch += f;
         if (((Entity) (Object) this).getVehicle() != null) {
@@ -46,7 +52,6 @@ public abstract class EntityMixin {
      */
     @Overwrite
     public void setPitch(float pitch) {
-
         /*if (pitch < -180) this.pitch = pitch + 360;
         else if (pitch > 180) this.pitch = pitch - 360;
         else */this.pitch = pitch;
