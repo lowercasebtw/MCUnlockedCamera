eaf0fc5e7af748e404fabde7662c7efd7669ad7a
kr1viah_ <kravpolet@gmail.com>
2025-03-25 10:19:23 +0100

    Cleaner fix for jump sprinting

--- src/client/java/org/kr1v/unlockedcamera/client/mixin/LivingEntityMixin.java
+++ src/client/java/org/kr1v/unlockedcamera/client/mixin/LivingEntityMixin.java
@@ -1,16 +1,14 @@
 package org.kr1v.unlockedcamera.client.mixin;
 
-import com.google.common.annotations.VisibleForTesting;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.kr1v.unlockedcamera.client.UnlockedCameraConfigManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
@@ -18,29 +16,22 @@ public abstract class LivingEntityMixin extends Entity {
         super(type, world);
     }
 
-    @Shadow
-    protected abstract float getJumpVelocity();
-    /**
-     * @author kr1v
-     * @reason make jumping while looking upside down speed you up instead of slow you down
-     */
-    @VisibleForTesting
-    @Overwrite
-    public void jump() {
-        float f = this.getJumpVelocity();
-        if (!(f <= 1.0E-5F)) {
-            Vec3d vec3d = this.getVelocity();
-            this.setVelocity(vec3d.x, Math.max(f, vec3d.y), vec3d.z);
-
-            if (this.isSprinting()) {
-                float normalizedPitch = ((this.getPitch() + 180) % 360 + 360) % 360 - 180;
-                float g = this.getYaw() * (float) (Math.PI / 180.0);
-                if ((normalizedPitch < -90 || normalizedPitch > 90) && UnlockedCameraConfigManager.getConfig().shouldInvertMovement)
-                    this.addVelocityInternal(new Vec3d(-MathHelper.sin(g) * -0.2, 0.0, MathHelper.cos(g) * -0.2));
-                else
-                    this.addVelocityInternal(new Vec3d(-MathHelper.sin(g) * 0.2, 0.0, MathHelper.cos(g) * 0.2));
-            }
-            this.velocityDirty = true;
-        }
+    @WrapOperation(
+            method = "jump",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;sin(F)F"))
+    public float sinWrap(float value, Operation<Float> original) {
+        float normalizedPitch = ((this.getPitch() + 180) % 360 + 360) % 360 - 180;
+        if ((normalizedPitch < -90 || normalizedPitch > 90) && UnlockedCameraConfigManager.getConfig().shouldInvertMovement)
+            return -original.call(value);
+        else return original.call(value);
+    }
+    @WrapOperation(
+            method = "jump",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;cos(F)F"))
+    public float cosWrap(float value, Operation<Float> original) {
+        float normalizedPitch = ((this.getPitch() + 180) % 360 + 360) % 360 - 180;
+        if ((normalizedPitch < -90 || normalizedPitch > 90) && UnlockedCameraConfigManager.getConfig().shouldInvertMovement)
+            return -original.call(value);
+        else return original.call(value);
     }
 }
--- src/client/java/org/kr1v/unlockedcamera/client/mixin/PlayerEntityMixin.java
+++ src/client/java/org/kr1v/unlockedcamera/client/mixin/PlayerEntityMixin.java
@@ -3,13 +3,12 @@ package org.kr1v.unlockedcamera.client.mixin;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.kr1v.unlockedcamera.client.UnlockedCameraConfigManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity {
@@ -21,34 +20,14 @@ public abstract class PlayerEntityMixin extends LivingEntity {
      * @author kr1v
      * @reason implement swimming upside down movement and looking upside down movement
      */
-    @Overwrite
-    public void travel(Vec3d movementInput) {
+    @ModifyVariable(method = "travel(Lnet/minecraft/util/math/Vec3d;)V", at = @At("HEAD"))
+    public Vec3d invertMovement(Vec3d movementInput) {
         float normalizedPitch = ((this.getPitch() + 180) % 360 + 360) % 360 - 180;
-        if (this.hasVehicle()) {
-            super.travel(movementInput);
-        } else {
-            if ((normalizedPitch > 90.0F || normalizedPitch < -90.0F) &&
-                    ((UnlockedCameraConfigManager.getConfig().shouldInvertMovementSwimming && this.isSwimming())
-                    || UnlockedCameraConfigManager.getConfig().shouldInvertMovement)) {
-                movementInput = movementInput.multiply(-1, 1, -1);
-            }
-
-            if (this.isSwimming()) {
-                double d = getRotationVector().y;
-                double e = d < -0.2 ? 0.085 : 0.06;
-                if (d <= 0.0 || this.jumping ||
-                        !this.getWorld().getFluidState(BlockPos.ofFloored(this.getX(), this.getY() + 0.9, this.getZ())).isEmpty()) {
-                    Vec3d vec3d = this.getVelocity();
-                    this.setVelocity(vec3d.add(0.0, (d - vec3d.y) * e, 0.0));
-                }
-            }
-            if (((PlayerEntity)(Object)this).getAbilities().flying) {
-                double d = this.getVelocity().y;
-                super.travel(movementInput);
-                this.setVelocity(this.getVelocity().withAxis(Direction.Axis.Y, d * 0.6));
-            } else {
-                super.travel(movementInput);
-            }
+        if ((normalizedPitch > 90.0F || normalizedPitch < -90.0F) &&
+                ((UnlockedCameraConfigManager.getConfig().shouldInvertMovementSwimming && this.isSwimming())
+                        || UnlockedCameraConfigManager.getConfig().shouldInvertMovement)) {
+            movementInput = movementInput.multiply(-1, 1, -1);
         }
+        return movementInput;
     }
 }
