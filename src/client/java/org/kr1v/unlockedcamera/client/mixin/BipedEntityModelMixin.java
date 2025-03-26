cd08337ebc493d4f97f15baea26fe442a78c89a6
kr1viah_ <kravpolet@gmail.com>
2025-03-26 11:28:08 +0100

    Fixes #1: don't return null in getHead()

--- src/client/java/org/kr1v/unlockedcamera/client/mixin/BipedEntityModelMixin.java
+++ src/client/java/org/kr1v/unlockedcamera/client/mixin/BipedEntityModelMixin.java
@@ -7,8 +7,6 @@ import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.render.entity.model.ModelWithArms;
 import net.minecraft.client.render.entity.model.ModelWithHead;
 import net.minecraft.client.render.entity.state.BipedEntityRenderState;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.Arm;
 import org.joml.Math;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,7 +14,7 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(BipedEntityModel.class)
-public class BipedEntityModelMixin<T extends BipedEntityRenderState> extends EntityModel<T> implements ModelWithArms, ModelWithHead {
+public abstract class BipedEntityModelMixin<T extends BipedEntityRenderState> extends EntityModel<T> implements ModelWithArms, ModelWithHead {
     protected BipedEntityModelMixin(ModelPart root) {
         super(root);
     }
@@ -39,22 +37,4 @@ public class BipedEntityModelMixin<T extends BipedEntityRenderState> extends Ent
         else if (normalizedPitch > pi/2) return pi-normalizedPitch;
         return normalizedPitch;
     }
-
-    // functions for extending stuff
-
-    @Override
-    public void setArmAngle(Arm arm, MatrixStack matrices) {
-
-    }
-
-    /**
-     * Gets the head model part.
-     *
-     * @return the head
-     */
-    @Override
-    public ModelPart getHead() {
-        return null;
-    }
-
 }
